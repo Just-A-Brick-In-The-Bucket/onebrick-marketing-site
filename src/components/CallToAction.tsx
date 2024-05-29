167544e0c2fa9d48033db0f64db29efa759adc6c
JustABrickInTheBucket <justabrickinthebucket@gmail.com>
2024-05-29 14:08:56 -0700

    refactor: Update call to action

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -21,14 +21,13 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Get started today
+            Complex Lots
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+          Effortlessly unite your unconsolidated inventory and create a seamless purchase experience for your customers.
           </p>
           <Button href="/register" color="white" className="mt-10">
-            Get 6 months free
+            Sign Up Now
           </Button>
         </div>
       </Container>
